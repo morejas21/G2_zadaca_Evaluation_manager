1e6ef5232376cc812f6ffacbd2cfe8f8b1f68d4f
Marin Orejaš <morejas21@foi.hr>
2023-05-20 22:13:49 +0200

    Three new classes have been created.

---
+++ G2_zadaca_EM/G2_zadaca_EM/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G2_zadaca_EM.Models
+{
+    public abstract class Person : object
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+}
---
+++ G2_zadaca_EM/G2_zadaca_EM/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G2_zadaca_EM.Models
+{
+    public class Student : Person
+    {
+        public int Grade { get; set; }
+    }
+
+}
