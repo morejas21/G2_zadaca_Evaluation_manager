a621d2c4358371e2da63c061d667836d135ec2a5
Marin Orejaš <morejas21@foi.hr>
2023-05-20 23:32:38 +0200

    Students display.

--- G2_zadaca_EM/G2_zadaca_EM/FrmStudents.cs
+++ G2_zadaca_EM/G2_zadaca_EM/FrmStudents.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using G2_zadaca_EM.Models;
+using G2_zadaca_EM.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +18,21 @@ namespace G2_zadaca_EM
         {
             InitializeComponent();
         }
+
+        private void FrmStudents_Load(object sender, EventArgs e)
+        {
+            ShowStudents();
+        }
+        private void ShowStudents()
+        {
+            List<Student> students = StudentRepository.GetStudents();
+            dgvStudents.DataSource = students;
+            dgvStudents.Columns["Id"].DisplayIndex = 0;
+            dgvStudents.Columns["FirstName"].DisplayIndex = 1;
+            dgvStudents.Columns["LastName"].DisplayIndex = 2;
+            dgvStudents.Columns["Grade"].DisplayIndex = 3;
+        }
+
+
     }
 }
